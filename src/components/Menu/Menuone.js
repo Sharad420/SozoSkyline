2c9f7e297f405204c022e081841062252014d321
Vrishank27 <vrishankshishir@gmail.com>
2024-06-30 19:35:04 +0530

    Edited menuone page and added necessary resources

--- src/components/Menu/Menuone.js
+++ src/components/Menu/Menuone.js
@@ -4,11 +4,12 @@ import Offer from '../Allmenus/FirstMenu/Offers'
 import Reservation from '../Allmenus/FirstMenu/Reservation';
 import Bredcom from '../Bredcrumb/Main'
 import Img from '../../assets/images/background/banner-image-2.jpg'
+import menu_img from '../../assets/images/background/bg_img_menu.png'
 
 function Menuone() {
   return (
     <>
-      <Bredcom subtitle="Our Menu" title="DELICIOUS & AMAZING" Img={Img} />
+      <Bredcom subtitle="Our Menu" title="DELICIOUS & AMAZING" Img={menu_img} />
       <Startermenu />
       <Offer />
       <Reservation />
